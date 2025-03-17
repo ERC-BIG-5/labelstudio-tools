3683af9b408b2d55d5cdec9a379ed9f4692028c7
rsoleyma <ramin.soleymani@bsc.es>
2025-03-17 14:20:44 +0100

    project-data dl

--- main.py
+++ main.py
@@ -192,6 +192,8 @@ def download_project_data(
         platform: Annotated[str, typer.Argument()],
         language: Annotated[str, typer.Argument()]
 ):
+    project_info = ProjectOverview.project_data(platform, language)
+    project_id = project_info["id"]
     project_data = ls_client().get_project(project_id)
     if not project_data:
         raise ValueError(f"No project found: {project_id}")
